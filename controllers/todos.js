b912c5cf92a22f6231c3cdda30968d493136aaa8
wnf <william@fluxrme.com>
2023-10-18 14:03:32 -0500

    completes ejs partials lesson

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,20 +1,22 @@
 // controllers/todos.js
 
+module.exports = {
+  index, show
+};
+
 // Convention is to name the model in uppercase and singular
 const Todo = require('../models/todo');
 
 function index(req, res) {
   res.render('todos/index', {
-    todos: Todo.getAll()
+    todos: Todo.getAll(),
+    title: 'All To-Dos'
   });
 }
 
 function show(req, res) {
   res.render('todos/show', {
     todo: Todo.getOne(req.params.id),
+    title: 'Details:'
   });
 }
-
-module.exports = {
-    index, show
-};
